815c860644d6d734f4ba095bd67b36b15264cc85
oskarfjo <oskarfjo@stud.ntnu.no>
2024-09-09 15:21:50 +0200

    Fiksa shit

--- SimpeShipSim/ShipModel.py
+++ SimpeShipSim/ShipModel.py
@@ -8,7 +8,7 @@ class Ship:
         # Dynamic states
         self.position    = 0.0
         self.velocity    = 0.0
-        self.disturbance = random.randint(-15, 15)
+        self.disturbance = 20 #random.randint(-15, 15)
         
         # Vessel parameters
         self.mass              = 10
@@ -16,58 +16,72 @@ class Ship:
         self.Area              = 0.04 
         self.propellerDiameter = 0.08
         self.KT0               = 0.4
-        self.rho               = 1000
+        self.rho               = 1025
 
         # User inputs
         self.control_active = False
         self.setpoint       = 0
         self.rps            = 0
         self.max_rps        = 15000 / 60
+        self.max_thrust     = 0.5 * self.rho * self.KT0 * self.propellerDiameter ** 4 * abs(self.max_rps) * self.max_rps
         self.s              = 0
-
         self.q_i            = 0
         self.v              = 0
+        self.e              = 1
+        self.eps            = 0.5
 
     def PID(self, dt):
 
-        omega = 1.5
+        omega = 1.8
         zeta = 1
         m = self.mass
         d_stjerne = 1/2 * self.rho * self.CD0 * self.Area * 0.5
 
         error = self.setpoint - self.position
-        error_a = error
         error_dot = 0 - self.velocity
 
         k_p = m * omega ** 2
         k_d = m * 2 * zeta * omega - d_stjerne
-        k_i = k_p / 10
+        k_i = k_p / (5 + error ** 2)
 
-        if error_a > 1:
-            error_a = 1
-        elif error_a < -1:
-            error_a = -1
+        if error > 1:
+            self.e = 1
+        elif error < -1:
+            self.e = -1
+        else:
+            self.e = error
 
-        self.q_i = self.q_i + dt * error_a 
+        #if self.q_i > -self.eps * self.max_thrust and self.e < 0:
+        #    self.q_i += k_i * dt * self.e
 
-        u = k_p * error + k_d * error_dot + k_i * self.q_i
+        #elif self.q_i < self.eps * self.max_thrust and self.e > 0:
+        #    self.q_i += k_i * dt * self.e
+        
+        if (self.q_i > self.eps * self.max_thrust) and (self.e > 0) or (self.q_i < - self.eps * self.max_thrust) and (self.e < 0):
+            self.q_i += 0
 
-        if u > self.max_rps:
-            u = self.max_rps
-        elif u < -self.max_rps:
-            u = -self.max_rps
+        else:
+            self.q_i += self.e * dt
 
-        self.rps = u   
+        P_ledd = k_d * error
+        D_ledd = k_d * error_dot
+        I_ledd = k_i * self.q_i
+
+
+        u = P_ledd + D_ledd + I_ledd
 
         if (self.s < 1):
 
             A = np.array([[0, 1],
-              [-k_p / m, -(k_d + 2 * d_stjerne) / m]])
+              [-k_p / m, -(k_d + d_stjerne) / m]])
     
             eigen = np.linalg.eigvals(A)
-            print("eigenverdier", eigen)
+            print("eigenverdier: ", eigen)
             self.s += 1
-
+        
+        print(f'P leddet: {P_ledd: .2f}, D leddet: {D_ledd: .2f}, og I leddet: {I_ledd: .2f}. u blir da {u: .2f}')
+        
+        return u
 
 
     
@@ -92,13 +106,19 @@ class Ship:
     def update_dynamics(self, dt):
 
         if self.control_active:
-            self.PID(dt)
+            U = self.PID(dt)
             # self.super_twist(dt)
+            self.rps = np.sign(U) * np.sqrt((2 * np.abs(U)) / ( self.rho * self.KT0 * (self.propellerDiameter ** 4)))
 
-        # thrust = 0.5 * self.rho * self.KT0 * self.propellerDiameter ** 4 * abs(self.rps) * self.rps
-        # drag = -0.5 * self.rho * self.CD0 * self.Area * abs(self.velocity) * self.velocity
+        if self.rps > self.max_rps:
+            self.rps = self.max_rps
+        elif self.rps < -self.max_rps:
+            self.rps = -self.max_rps
+        
+        thrust = 0.5 * self.rho * self.KT0 * self.propellerDiameter ** 4 * abs(self.rps) * self.rps
+        drag = -0.5 * self.rho * self.CD0 * self.Area * abs(self.velocity) * self.velocity
 
-        self.velocity = self.velocity + dt * ((1 / self.mass) * (self.disturbance + (1/2) * self.rho * self.KT0 * self.propellerDiameter ** 4 * abs(self.rps) * self.rps - (1/2) * self.rho * self.CD0 * self.Area * abs(self.velocity) * self.velocity ))
+        self.velocity = self.velocity + dt * ((1 / self.mass) * (thrust + drag + self.disturbance))
         self.position = self.position + dt * self.velocity
 
         return 0.0
